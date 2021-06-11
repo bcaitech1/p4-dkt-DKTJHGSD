b368900d15e93995ff1676854d613864d54ade2a
junho621 <junho621@gmail.com>
2021-06-11 08:56:10 +0000

    <feat> Add LANA

--- code/dkt/model.py
+++ code/dkt/model.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import numpy as np
 import os
-import re
 
 try:
     from transformers.modeling_bert import BertConfig, BertEncoder, BertModel    
